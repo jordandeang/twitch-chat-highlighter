d4e7b86c1cd35773da882c9fcc10a252e1722da6
Jordan Deang <jordandeang@debian>
2016-07-04 02:37:50 -0400

    Chat graph representation

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests, sys, time, json
+import requests, sys, datetime, time, json
+import plotly as py
+import plotly.graph_objs as go
 
 # Video id input (prefixed 'v' is optional)
 videoId = 'v' + sys.argv[1].replace('v', '')
@@ -15,15 +17,58 @@ detail = response['errors'][0]['detail'].split(' ')
 start = int(detail[4])
 stop = int(detail[6])
 
-# Used message ids
-#messageIds = []
+# List of tuples of format (VOD Time, number of twitch chat messages)
+concentrationByTime = []
 
 # For every 30 seconds, start to stop; request chat messages
-for timestamp in xrange(start, stop, 30):
+for timestamp in xrange(start, start+2000, 30):
 
 	# Request messages from Twitch
 	response = requests.get('https://rechat.twitch.tv/rechat-messages?start=' + str(timestamp) + '&video_id=' + videoId).json()
 	data = response['data']
 
-	print len(data)
-	print time.strftime('%m/%d/%Y %H:%M:%S',  time.gmtime(timestamp))
+
+	concentrationByTime.append(
+		(str(datetime.timedelta(seconds = (timestamp - start))), len(data))
+	)
+
+	sys.stdout.write("\r%d%%" % ( (float(timestamp - start) / float(stop- start)) * 100))
+	sys.stdout.flush() 
+
+# Plotly stuff
+x = [a[0] for a in concentrationByTime]
+y = [a[1] for a in concentrationByTime]
+yText = [str(a) + ' messages' for a in y]
+
+trace1 = go.Bar(
+	x = x,
+	y = y,
+	text =  yText,
+	hoverinfo = 'x+text',
+	marker=dict(
+		color='rgb(158,202,225)',
+		line=dict(
+			color='rgb(8,48,107)',
+			width=1,
+		)
+	),
+	opacity = 0.6
+)
+
+# Requests VOD title
+videoTitle = requests.get('https://api.twitch.tv/kraken/videos/' + videoId).json()['title']
+
+layout = go.Layout(
+	title = videoTitle,
+	bargap = 0,
+	xaxis = dict(
+		title = "VOD Time"
+	),
+	yaxis = dict(
+		title = "Number of Twitch Chat Messages"
+	)
+)
+
+plotData = [trace1]
+fig = go.Figure(data = plotData, layout = layout)
+py.offline.plot(fig)
