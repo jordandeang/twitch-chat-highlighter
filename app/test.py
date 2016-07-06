fce0f4aa2670e5841b7bba633a89bd334be4df38
Jordan Deang <jdeang@vt.edu>
2016-07-06 00:08:25 -0400

    AJAX stuff

---
+++ app/test.py
@@ -0,0 +1,4 @@
+import time
+
+time.sleep(3)
+return "cool python test " + str(args[1]);
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, make_response
 from app import app
 from .forms import VideoURLForm
-from .twitchChatParser import parseTwitchChat, getVideoIdFromURL
+from .twitchChatParser import getVideoIdFromURL, parseTwitchChat
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,7 +9,6 @@ def index():
 	form = VideoURLForm()
 	if form.validate_on_submit():
 		return redirect(url_for('vod', videoId=getVideoIdFromURL(form.videoURL.data)))
-
 	return render_template('index.html', 
 							title='Home',
 							form=form)
@@ -17,15 +16,21 @@ def index():
 @app.route('/v/<videoId>', methods=['GET', 'POST'])
 def vod(videoId):
 	form = VideoURLForm()
-	plotScript, plotDiv = parseTwitchChat(videoId)
-	vods = [
-		{
-			'videoId' : 'v'+videoId,
-			'plotScript' : plotScript,
-			'plotDiv' : plotDiv
-		}
-	]
+	if form.validate_on_submit():
+		return redirect(url_for('vod', videoId=getVideoIdFromURL(form.videoURL.data)))
 	return render_template('vod.html', 
 							title='Vod Highlight',
 							form=form,
-							vods=vods)
+							videoId=videoId)
+
+@app.route('/p', methods=['POST'])
+def chatParser():
+	if request.method == 'POST':
+		videoId = request.form['videoId']
+		plotScript, plotDiv = parseTwitchChat(videoId)
+		resp = make_response(plotScript+plotDiv)
+		resp.headers['Content-Type'] = 'text/html'
+		print videoId
+		return resp
+	else:
+		return redirect(url_for('vod', videoId=videoId))
